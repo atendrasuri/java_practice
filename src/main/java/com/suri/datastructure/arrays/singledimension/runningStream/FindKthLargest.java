09ddc281fb04b9b749034ae4e1464dc928e983c5
Atendra Kumar <akumar376@sapient.com>
2019-03-26 20:45:45 +0530

    done

--- src/main/java/com/suri/datastructure/arrays/singledimension/runningStream/FindKthLargest.java
+++ src/main/java/com/suri/datastructure/arrays/singledimension/runningStream/FindKthLargest.java
@@ -1,7 +1,5 @@
 package com.suri.datastructure.arrays.singledimension.runningStream;
 
-import java.util.PriorityQueue;
-
 /**
  * @Author: atekumar
  * @Current-Version: 1.0.0
@@ -15,28 +13,63 @@ public class FindKthLargest {
 
     public static void main(String[] args) {
 
-        int arr[]  = {1,5,3,6,2,9};
-        System.out.println(KthLargest(2,arr));
+        int arr[] = {14, 7, 10, 20, 5, 24};
+        System.out.println(""+findKth(arr,5));
+
     }
 
-     static PriorityQueue<Integer> q=null;
-     static int k;
+    public static int findKth(int arr[], int k ){
 
-    public static int KthLargest(int k, int[] a) {
+        heapSort(arr,k);
+        for(int i=k;i<arr.length;i++){
 
-        q = new PriorityQueue<>(k);
-        for (int n : a)
-             add(n);
+            if(arr[0]<arr[i]){
+                int temp= arr[0];
+                arr[0]= arr[i];
+                arr[i]=temp;
+                heapSort(arr,k);
+            }
+        }
 
-        return q.peek();
+        return arr[0];
     }
 
-    public static void add(int n) {
-        if (q.size() < k)
-            q.offer(n);
-        else if (q.peek() < n) {
-            q.poll();
-            q.offer(n);
+    /**
+     * building heap
+     * @param arr
+     * @param k
+     */
+    public static void heapSort(int arr[], int k){
+        for(int i=k/2-1;i>=0;i--){
+          heapify(arr,i,k);
+        }
+
+
+    }
+
+    /**
+     * heapify method
+     * @param arr
+     * @param i
+     * @param n
+     */
+    public static void heapify(int arr[], int i, int n){
+        int largest=i;
+        int l= 2*i+1;
+        int r= 2*i+2;
+
+        if(l<n && arr[l]<arr[largest]){
+            largest=l;
+        }
+        if(r<n && arr[r]<arr[largest]){
+            largest= r;
+        }
+
+        if(largest !=i){
+            int temp= arr[i];
+            arr[i]= arr[largest];
+            arr[largest]= temp;
+            heapify(arr,i,n);
         }
 
     }
