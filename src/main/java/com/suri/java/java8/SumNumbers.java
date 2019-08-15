1c63a7bf228e3304527abae8698c9f9cfd0650be
atendra.kumar@publicissapient.com <ATENDRA_KUMAR@CRGL-THIRDPARTY.COM>
2019-08-15 23:18:52 +0530

    added some Q

---
+++ src/main/java/com/suri/java/java8/SumNumbers.java
@@ -0,0 +1,43 @@
+package com.suri.java.java8;
+
+import java.util.stream.IntStream;
+
+/**
+ * @Author: atekumar
+ * @Current-Version: 1.0.0
+ * @Creation-Date: 15/08/19
+ * @Description: (Overwrite)
+ * 1. Please describe the business usage of the class.
+ * 2. Please describe the technical usage of the class.
+ * @History:
+ * Program to sum all the numbers divisible by 3 and 5 for a given number
+ * Given the integer N, the task is to sum all the numbers less than N, which are divisible by 3 and 5.
+ *
+ * Examples :
+ *
+ * Input : 50
+ * Output : 0 15 30 45
+ *
+ * Input : 100
+ * Output : 0 15 30 45 60 75 90
+ */
+public class SumNumbers {
+  public static void main(String[] args) {
+
+    System.out.println(findSum(100));
+    System.out.println(count(100));
+
+  }
+
+  public static int findSum(int nVal){
+
+
+    int sum= IntStream.range(1,nVal).filter(x->x%3==0|| x%5==0).sum();
+    return sum;
+  }
+  public static Long count(int nVal){
+
+    Long count= IntStream.range(1,nVal).filter(x->x%3==0|| x%5==0).count();
+    return count;
+  }
+}
